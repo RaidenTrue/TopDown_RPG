aa8854f277612a1478d8ddc8a82a0cb11f8fb991
RaidenTrue <raidmgs@gmail.com>
2026-03-06 16:13:07 +0200

    WeaponSocket for BP_RPG Character & BP_Enemy

--- Source/TDRPG/Private/Character/RPGCharacterBase.cpp
+++ Source/TDRPG/Private/Character/RPGCharacterBase.cpp
@@ -7,6 +7,9 @@ ARPGCharacterBase::ARPGCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ARPGCharacterBase::BeginPlay()
