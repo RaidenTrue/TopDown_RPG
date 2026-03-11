89d5ce2a7b854a6c8197ea2da9f4bef022ea2ff4
RaidenTrue <raidmgs@gmail.com>
2026-03-11 03:39:07 +0200

    RPG PlayerState for AbilitySystemComponent & AttributeSet

---
+++ Source/TDRPG/Private/Player/RPGPlayerState.cpp
@@ -0,0 +1,10 @@
+// This Project is Copyrighted as property of Savvas Stones.
+
+
+#include "Player/RPGPlayerState.h"
+#include "Player/RPGPlayerState.h"
+
+ARPGPlayerState::ARPGPlayerState()
+{
+	NetUpdateFrequency = 100;
+}
---
+++ Source/TDRPG/Public/Player/RPGPlayerState.h
@@ -0,0 +1,21 @@
+// This Project is Copyrighted as property of Savvas Stones.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "RPGPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TDRPG_API ARPGPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+
+	ARPGPlayerState();
+	
+};
